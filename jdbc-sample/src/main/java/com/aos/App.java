594a007394aac9bfba34789e9e832e8f7c9b8694
Arilson Souza <arilsons61@gmail.com>
2024-09-01 17:18:57 -0300

    feat: setup flyway

--- jdbc-sample/src/main/java/com/aos/App.java
+++ jdbc-sample/src/main/java/com/aos/App.java
@@ -1,11 +1,21 @@
 package com.aos;
 
+import org.flywaydb.core.Flyway;
+
+import com.aos.persistence.entity.EmployeeDAO;
+
 /**
  * Hello world!
  *
  */
 public class App {
+    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var flyway = Flyway.configure()
+                .dataSource("jdbc:postgresql://localhost:5432/jdbc_sample", "postgres", "postgres")
+                .load();
+
+        flyway.migrate();
     }
 }
