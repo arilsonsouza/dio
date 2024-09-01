a748d5d1d2514801673550cf86ff9d5679370b7e
Arilson Souza <arilsons61@gmail.com>
2024-09-01 18:21:54 -0300

    feat: add Employee entity

---
+++ jdbc-sample/src/main/java/com/aos/persistence/entity/Employee.java
@@ -0,0 +1,19 @@
+package com.aos.persistence.entity;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+  private long id;
+
+  private String name;
+
+  private BigDecimal salary;
+
+  private OffsetDateTime birthday;
+
+}
