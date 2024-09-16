d5dc1bf796aa3f7c13031078356e8048915fa5af
Arilson Souza <arilsons61@gmail.com>
2024-09-16 07:18:35 -0300

    feat: angular modules

--- angular/diretivas-proj/src/app/app.component.ts
+++ angular/diretivas-proj/src/app/app.component.ts
@@ -4,11 +4,18 @@ import { CardComponent } from './card/card.component';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, NgIf, CompAtributosComponent],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    NgIf,
+    CompAtributosComponent,
+    SharedModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
